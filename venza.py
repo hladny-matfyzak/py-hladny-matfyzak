e2055acb83fe8b11af6e6b501adf4b4283067cee
mr.Shu <mr@shu.io>
2015-04-01 01:24:11 +0200

    janitoring: Refactored venza.py
    * Did some basic formatting changes via autopep8.
    
    * Refactored HS_URL into what I believe it should have been.
    
    * Added isdigit() check for provided days/months/years. Defaults to
      current values.
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- venza.py
+++ venza.py
@@ -3,63 +3,66 @@ import urllib2
 import re
 import time
 import datetime
-HS_URL = "http://hladnystudent.zones.sk"
+HS_URL = "http://hladnystudent.zones.sk/jedalne-listky-{}-{}-{}"
 
-def horna(day = None, month = None, year = None):
-    if year == None:
-        year = time.strftime("%Y")
-    if month == None:
-        month = time.strftime("%m")
-    if day == None:
-        day = time.strftime("%d")
-    lin =HS_URL.format("/jedalne-listky-",day,month,year)
-    url = urllib2.urlopen(lin)        
 
-    
+def horna(day=None, month=None, year=None):
+    if year is None and year.isdigit():
+        year = int(time.strftime("%Y"))
+    if day is None and day.isdigit():
+        day = int(time.strftime("%d"))
+    if month is None and day.isdigit():
+        month = int(time.strftime("%m"))
+
+    lin = HS_URL.format(day, month, year)
+    url = urllib2.urlopen(lin)
+
     text = url.read()
     soup = BeautifulSoup(text)
     tables = soup.find_all('table')
-    hornasoup = BeautifulSoup(str(tables[1])) 
+    hornasoup = BeautifulSoup(str(tables[1]))
     list = []
     trs = hornasoup.find_all('tr')
 
-    for i in range (1,len(trs)):        
-         td = str(trs[i])
-         tdsoup = BeautifulSoup(td)
-         bordel = tdsoup.find_all('td')
-         match = re.findall(r'<td>(.*?)<span',str(bordel[1]),re.DOTALL)
-         bettermatch = re.findall(r'\n\s\s\s\s\s\s\s\s(.*?)\n',match[0])
-         list.append(bettermatch[0])
+    for i in range(1, len(trs)):
+        td = str(trs[i])
+        tdsoup = BeautifulSoup(td)
+        bordel = tdsoup.find_all('td')
+        match = re.findall(r'<td>(.*?)<span', str(bordel[1]), re.DOTALL)
+        bettermatch = re.findall(r'\n\s\s\s\s\s\s\s\s(.*?)\n', match[0])
+        list.append(bettermatch[0])
     return list
-    
-def dolna(day = None, month = None, year = None):
-    list= []
-    if year == None:
-        year = (int)(time.strftime("%Y"))
-    if day == None:
-        day = (int)(time.strftime("%d"))
-    if month == None:
-        month = (int)(time.strftime("%m"))
-    lin =HS_URL.format("/jedalne-listky-",day,month,year)
-    weekday = (datetime.date(year,month,day)).weekday()
+
+
+def dolna(day=None, month=None, year=None):
+    list = []
+    if year is None and year.isdigit():
+        year = int(time.strftime("%Y"))
+    if day is None and day.isdigit():
+        day = int(time.strftime("%d"))
+    if month is None and day.isdigit():
+        month = int(time.strftime("%m"))
+
+    lin = HS_URL.format(day, month, year)
+    weekday = (datetime.date(year, month, day)).weekday()
     weekday = int(weekday)
     if weekday == 6 or weekday == 5:
         list.append("Closed")
         return list
     url = urllib2.urlopen(lin)
-        
+
     text = url.read()
     soup = BeautifulSoup(text)
     tables = soup.find_all('table')
-    dolnasoup = BeautifulSoup(str(tables[2])) 
+    dolnasoup = BeautifulSoup(str(tables[2]))
     trs_dolna = dolnasoup.find_all('tr')
 
-    for i in range (1,trs_dolna.__len__()):        
+    for i in range(1, len(trs_dolna)):
         dtd = str(trs_dolna[i])
         dtdsoup = BeautifulSoup(dtd)
         dbordel = dtdsoup.find_all('td')
 
-        dmatch = re.findall(r'<td>(.*?)<span',str(dbordel[1]),re.DOTALL)
-        dbettermatch = re.findall(r'\n\s\s\s\s\s\s\s\s(.*?)\n',dmatch[0])
+        dmatch = re.findall(r'<td>(.*?)<span', str(dbordel[1]), re.DOTALL)
+        dbettermatch = re.findall(r'\n\s\s\s\s\s\s\s\s(.*?)\n', dmatch[0])
         list.append(dbettermatch[0])
     return list
