a3da4433c0606bd19781d720762b4ba5bfdb79e9
Wido <tomas.wido@gmail.com>
2015-09-22 19:58:25 +0200

    added setup.py for pip

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='hladny_matfyzak',
+
+    version='0.0.5',
+
+    description='Python parsing functions for hungry students in Bratislava',
+
+    url='https://github.com/hladny-matfyzak/py-hladny-matfyzak',
+
+    author='Wido',
+
+    author_email='tomas.wido@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+    ],
+    
+    packages=["hladnymatfyzak"],
+
+    install_requires=['requests', 'BeautifulSoup', 'enum34'],
+)
