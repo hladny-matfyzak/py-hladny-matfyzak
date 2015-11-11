6d2c7161b2f46b2299e0cc94792c3906013a74ca
Wido <tomas.wido@gmail.com>
2015-11-11 14:06:39 +0100

    Repaired parsing for ffood and dolna Website of venza changed order in table. For now it's fixed. Weekday for dolna was 1 day late, now just increments value by 1.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='hladny_matfyzak',
 
-    version='0.0.6.1',
+    version='0.0.7.0',
 
     description='Python parsing functions for hungry students in Bratislava',
 
